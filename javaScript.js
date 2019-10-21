f3b8f64a6fbb8935cabefa822ea43a53ac0bbf1d
albinnilssonafsillen <32542320+albinnilssonafsillen@users.noreply.github.com>
2019-10-21 14:53:04 +0200

    Add files via upload
    First Commit in new project: WebSite

---
+++ javaScript.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.slicker').slick({
+        autoplay: true,
+        speed: 300,
+        infinite: true,
+        prevArrow: false,
+        nextArrow: false,
+        cssEase: 'linear'
+    });
+
+});
