bb0e0266fe07faa21a32e978f5da3da39b5746a1
albinnilssonafsillen <32542320+albinnilssonafsillen@users.noreply.github.com>
2021-03-09 13:31:39 +0100

    changes lol

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
     $('.slicker').slick({
         autoplay: true,
-        speed: 300,
+        speed: 200,
         infinite: true,
+        fade: true,
         prevArrow: false,
         nextArrow: false,
         cssEase: 'linear'
     });
-
 });
+
+function justSayYes() {
+
+    const response = confirm("Vänligen bekräfta om du vill ingå i partnerskap?");
+
+    if(response) {
+        alert(`Nice`)
+        document.getElementById("theLoveDiv").style.visibility = "visible";
+    }
+}
